88f2208b7a7aa52b69bfaf8821c8aa51264e74eb
zvher <zuher.feki17@gmail.com>
2022-04-12 00:24:43 +1000

    deploy

--- server.js
+++ server.js
@@ -104,6 +104,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'build')));
   
     app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'build', 'App.js'));
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
   }
